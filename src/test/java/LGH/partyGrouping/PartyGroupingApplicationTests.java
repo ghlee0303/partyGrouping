57c715f595b6996ad5a5d3d61b7bfdeeadb48e16
ghlee0303 <rjs993530@gmail.com>
2023-02-18 04:12:48 +0900

    first commit

---
+++ src/test/java/LGH/partyGrouping/PartyGroupingApplicationTests.java
@@ -0,0 +1,13 @@
+package LGH.partyGrouping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyGroupingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

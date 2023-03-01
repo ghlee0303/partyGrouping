2e7ea28335c6dc8127231de54f0d0f79c85ca5d1
ghlee0303 <rjs993530@gmail.com>
2023-03-01 12:57:37 +0900

    refactor: config 일원화

--- src/main/java/com/party_grouping/config/SpringConfig.java
+++ src/main/java/com/party_grouping/config/SpringConfig.java
@@ -1,5 +1,9 @@
 package com.party_grouping.config;
 
+import com.party_grouping.repository.CharacterRepo;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -7,6 +11,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringConfig {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+
+    @Bean
+    public CharacterRepo characterResp() {
+        return new CharacterRepo(jpaQueryFactory());
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();

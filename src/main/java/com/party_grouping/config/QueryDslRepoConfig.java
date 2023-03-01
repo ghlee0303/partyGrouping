5139b980902b936559b0dbbe32c9dad810dd44ee
ghlee0303 <rjs993530@gmail.com>
2023-03-01 11:22:49 +0900

    feat: config 추가

---
+++ src/main/java/com/party_grouping/config/QueryDslRepoConfig.java
@@ -0,0 +1,25 @@
+package com.party_grouping.config;
+
+import com.party_grouping.repository.CharacterRepo;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslRepoConfig {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+
+    @Bean
+    public CharacterRepo characterResp() {
+        return new CharacterRepo(jpaQueryFactory());
+    }
+
+}

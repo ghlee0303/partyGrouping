8172c7524c549969bec52dad1498329eab1abc98
ghlee0303 <rjs993530@gmail.com>
2023-03-06 18:02:53 +0900

    feat: index 페이지 추가

--- src/main/java/com/party_grouping/controller/mainController.java
+++ src/main/java/com/party_grouping/controller/mainController.java
@@ -1,15 +1,22 @@
 package com.party_grouping.controller;
 
+import com.party_grouping.service.CharacterService;
+import com.party_grouping.service.DungeonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class mainController {
+    @Autowired
+    private CharacterService characterService;
+    @Autowired
+    private DungeonService dungeonService;
 
     @GetMapping("/")
     public String getMain() {
-        String test = "1234";
 
-        return test;
+        return "index";
     }
+
 }

27e82caef848c9de0f590ea07985d1faffc9e8d8
ghlee0303 <rjs993530@gmail.com>
2023-03-21 11:14:45 +0900

    feat: TestPage Update

--- src/main/java/com/party_grouping/controller/mainController.java
+++ src/main/java/com/party_grouping/controller/mainController.java
@@ -2,7 +2,11 @@ package com.party_grouping.controller;
 
 import com.party_grouping.dto.*;
 import com.party_grouping.entity.PartyAndCharacterEntity;
+import com.party_grouping.request.CADRequestDto;
+import com.party_grouping.request.PACRequestDto;
+import com.party_grouping.request.PartyRequestDto;
 import com.party_grouping.response.CustomResponse;
+import com.party_grouping.response.dto.PACResponseDto;
 import com.party_grouping.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,6 +27,8 @@ public class mainController {
     private PartyService partyService;
     @Autowired
     private PartyAndCharacterService partyAndCharacterService;
+    @Autowired
+    private CharacterAndDungeonService characterAndDungeonService;
 
     @GetMapping("/")
     public String getMain() {
@@ -41,47 +47,56 @@ public class mainController {
 
     @GetMapping("pac_list")
     @ResponseBody
-    public List<List<PartyAndCharacterDto>> getPAC_List(@RequestParam(value = "partyId") Integer partyId) {
-        return partyAndCharacterService.doublePACListDto(partyId);
+    public CustomResponse<List<List<PACResponseDto>>> getPAC_List(@RequestParam(value = "partyId") Integer partyId) {
+        return new CustomResponse<>(partyAndCharacterService.doublePACResponseDtoList(partyId));
     }
 
     @PostMapping("character_insert")
     @ResponseBody
-    public CustomResponse<String> postCharacter_Insert(@RequestBody CharacterDto characterDto) {
+    public CustomResponse<Integer> postCharacter_Insert(@RequestBody CharacterDto characterDto) {
         System.out.println("캐릭터");
         characterService.save(characterDto);
-        return new CustomResponse<>(characterDto.getName());
+        return new CustomResponse<>(characterDto.getId());
     }
 
     @PostMapping("dungeon_insert")
     @ResponseBody
-    public CustomResponse<String> postDungeon_Insert(@RequestBody DungeonDto dungeonDto) {
+    public CustomResponse<Integer> postDungeon_Insert(@RequestBody DungeonDto dungeonDto) {
         System.out.println("던전");
         dungeonService.save(dungeonDto);
-        return new CustomResponse<>(dungeonDto.getName());
+        return new CustomResponse<>(dungeonDto.getId());
     }
 
     @PostMapping("group_insert")
     @ResponseBody
-    public CustomResponse<String> postGroup_Insert(@RequestBody GroupDto groupDto) {
+    public CustomResponse<Integer> postGroup_Insert(@RequestBody GroupDto groupDto) {
         System.out.println("그룹");
         groupService.save(groupDto);
-        return new CustomResponse<>(groupDto.getName());
+        return new CustomResponse<>(groupDto.getId());
     }
 
     @PostMapping("party_insert")
     @ResponseBody
-    public CustomResponse<String> postCharacter_Insert(@RequestBody PartyDto partyDto) {
+    public CustomResponse<Integer> postCharacter_Insert(@RequestBody PartyRequestDto partyRequestDto) {
         System.out.println("파티");
-        partyService.save(partyDto);
-        return new CustomResponse<>(partyDto.getName());
+        Integer partyId = partyService.save(partyRequestDto);
+        return new CustomResponse<>(partyId);
     }
 
     @PostMapping("pac_insert")
     @ResponseBody
-    public CustomResponse<Integer> postPAC_Insert(@RequestBody PartyAndCharacterDto partyAndCharacterDto) {
+    public CustomResponse<Integer> postPAC_Insert(@RequestBody PACRequestDto pacRequestDto) {
         System.out.println("파티원 등록");
-        partyAndCharacterService.save(partyAndCharacterDto);
-        return new CustomResponse<>(partyAndCharacterDto.getId());
+        Integer saveId = partyAndCharacterService.save(pacRequestDto);
+        return new CustomResponse<>(saveId);
+    }
+
+    @PostMapping("cad_insert")
+    @ResponseBody
+    public CustomResponse<Integer> postCAD_Insert(@RequestBody CADRequestDto cadRequestDto) {
+        System.out.println("던전 클리어 여부");
+        Integer cadId = characterAndDungeonService.save(cadRequestDto);
+
+        return new CustomResponse<>(cadId);
     }
 }

33ecc478fa0a9698e1a7e3a919910b3eaaf843f3
ghlee0303 <rjs993530@gmail.com>
2023-03-21 11:14:37 +0900

    feat: TestPage Update

--- src/main/resources/static/script/dataInsert.js
+++ src/main/resources/static/script/dataInsert.js
@@ -2,19 +2,20 @@ import * as fetchHandler from 'fetchHandler';
 
 window.onload = function () {
     eventBinding();
+    PAC_setting();
     console.log("오케이");
 }
 
 function characterInsert() {
     const serverUri = "/character_insert";
-    const name = document.getElementById("characterName").value;
-    const level = document.getElementById("characterLevel").value;
-    console.log(name + " / " + level);
+    const character_name = document.getElementById("characterName").value;
+    const character_level = document.getElementById("characterLevel").value;
+    console.log(character_name + " / " + character_level);
     const options = {
         method : 'POST',
         cache : 'no-cache',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ name, level })
+        body: JSON.stringify({ character_name, character_level })
     }
 
     fetch(serverUri, options)
@@ -30,16 +31,16 @@ function characterInsert() {
 
 function dungeonInsert() {
     const serverUri = "/dungeon_insert";
-    const name = document.getElementById("dungeonName").value;
-    const code = document.getElementById("dungeonCode").value;
-    const description = document.getElementById("dungeonDescription").value;
-    const levelLimit = document.getElementById("dungeonLevelLimit").value;
-    console.log(name + " / " + code+ " / " + description+ " / " + levelLimit);
+    const dungeon_name = document.getElementById("dungeonName").value;
+    const dungeon_code = document.getElementById("dungeonCode").value;
+    const dungeon_description = document.getElementById("dungeonDescription").value;
+    const dungeon_level_limit = document.getElementById("dungeonLevelLimit").value;
+    console.log(dungeon_name + " / " + dungeon_code+ " / " + dungeon_description+ " / " + dungeon_level_limit);
     const options = {
         method : 'POST',
         cache : 'no-cache',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ name, code, description, levelLimit })
+        body: JSON.stringify({ dungeon_name, dungeon_code, dungeon_description, dungeon_level_limit })
     }
 
     fetch(serverUri, options)
@@ -55,13 +56,13 @@ function dungeonInsert() {
 
 function groupInsert() {
     const serverUri = "/group_insert";
-    const name = document.getElementById("groupName").value;
-    console.log(name);
+    const group_name = document.getElementById("groupName").value;
+    console.log(group_name);
     const options = {
         method : 'POST',
         cache : 'no-cache',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({name})
+        body: JSON.stringify({group_name})
     }
 
     fetch(serverUri, options)
@@ -77,14 +78,14 @@ function groupInsert() {
 
 function partyInsert() {
     const serverUri = "/party_insert";
-    const name = document.getElementById("partyName").value;
+    const party_name = document.getElementById("partyName").value;
     const party_group_id = document.getElementById("partyGroup").value;
-    console.log(name);
+    console.log(party_name);
     const options = {
         method : 'POST',
         cache : 'no-cache',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({name, party_group_id})
+        body: JSON.stringify({party_name, party_group_id})
     }
 
     fetch(serverUri, options)
@@ -121,10 +122,99 @@ function PACInsert() {
             alert(err.res.data.message);
         });
 }
+
+function PAC_setting() {
+    const pac_tag_list = document.querySelectorAll(".pac-party-list");
+    console.log(pac_tag_list);
+    console.log(pac_tag_list.length);
+    pac_tag_list.forEach(pac_tag => {
+        PACCall(pac_tag);
+    })
+}
+
+function PACCall(pac_tag) {
+    const serverUri = "/pac_list?partyId="+pac_tag.getAttribute("value");
+
+    fetch(serverUri)
+        .then((res) => fetchHandler.toJsonPromise(res))
+        .then((res) => fetchHandler.httpStatusHandler(res) )
+        .then((res) => {
+            console.log(res);
+            res.forEach((pac_data_list, i) => {
+                if (pac_data_list?.length) {
+                    partyTable(pac_tag, pac_data_list, i+1);
+                }
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function partyTable(pac_tag, pac_data_list, partyNumber) {
+    const tableHeadTH = document.createElement("th");
+    tableHeadTH.innerText = partyNumber + "번 파티";
+    const tableHeadTR = document.createElement("tr");
+    tableHeadTR.appendChild(tableHeadTH);
+    const tableHead = document.createElement("thead");
+    tableHead.appendChild(tableHeadTR);
+
+    let tableBodyTR;
+    const tableBody = document.createElement("tbody");
+
+    pac_data_list.forEach(pac_data => {
+        tableBodyTR = document.createElement("tr");
+
+        const tableBodyTDName = document.createElement("td");
+        const tableBodyTDLevel = document.createElement("td");
+        const tableBodyTDId = document.createElement("td");
+        tableBodyTDName.innerText = pac_data.character.character_name;
+        tableBodyTR.appendChild(tableBodyTDName);
+
+        tableBodyTDLevel.innerText = pac_data.character.character_level;
+        tableBodyTR.appendChild(tableBodyTDLevel);
+
+        tableBodyTDId.innerText = pac_data.character.id;
+        tableBodyTR.appendChild(tableBodyTDId);
+
+        tableBody.appendChild(tableBodyTR);
+    });
+
+    const table = document.createElement("table");
+    table.appendChild(tableHead);
+    table.appendChild(tableBody);
+    pac_tag.appendChild(table);
+}
+
+function CADInsert() {
+    const serverUri = "/cad_insert";
+    const cad_dungeon_id = document.getElementById("cadDungeon").value;
+    const cad_character_id = document.getElementById("cadCharacter").value;
+    const cad_clear = document.getElementById("cadClear").checked;
+
+    const options = {
+        method : 'POST',
+        cache : 'no-cache',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({cad_dungeon_id, cad_character_id, cad_clear})
+    }
+
+    fetch(serverUri, options)
+        .then((res) => fetchHandler.toJsonPromise(res))
+        .then((res) => fetchHandler.httpStatusHandler(res) )
+        .then((res) => {
+            console.log("ok");
+        })
+        .catch((err) => {
+            alert(err.res.data.message);
+        });
+}
+
 function eventBinding() {
     document.getElementById("character").addEventListener("click", characterInsert);
     document.getElementById("dungeon").addEventListener("click", dungeonInsert);
     document.getElementById("group").addEventListener("click", groupInsert);
     document.getElementById("party").addEventListener("click", partyInsert);
     document.getElementById("pac").addEventListener("click", PACInsert);
+    document.getElementById("cad").addEventListener("click", CADInsert);
 }

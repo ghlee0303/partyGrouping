d5bd57a9d98ecf147ee3a77e09b36a95d3ee904d
ghlee0303 <rjs993530@gmail.com>
2023-04-21 04:39:51 +0900

    feat: ApiException 간략화

--- src/main/java/com/party_grouping/exception/ApiException.java
+++ src/main/java/com/party_grouping/exception/ApiException.java
@@ -7,28 +7,15 @@ import java.util.Map;
 
 public class ApiException extends RuntimeException {
     protected Integer err_code;
-    protected ResponseEntity<String> response;
-    protected String test;
 
     public ApiException() {
     }
 
-    public ApiException(String message, Integer err_code, String test) {
-        super(message);
-        this.err_code = err_code;
-        this.test = test;
-
-    }
     public ApiException(String message, Integer err_code) {
         super(message);
         this.err_code = err_code;
     }
 
-    public ApiException(String message, Integer err_code, ResponseEntity<String> response) {
-        super(message);
-        this.err_code = err_code;
-        this.response = response;
-    }
 
     public Integer getErr_code() {
         return err_code;
@@ -36,9 +23,5 @@ public class ApiException extends RuntimeException {
 
     public void setErr_code(Integer err_code) {
         this.err_code = err_code;
-    }
-
-    public Map<String, Object> responseMapping() {
-        return response == null ? null : new JSONObject(response.getBody()).toMap();
     }
 }

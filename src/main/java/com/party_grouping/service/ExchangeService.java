2800e543d60be5f7f4afbac62170db528743d33a
ghlee0303 <rjs993530@gmail.com>
2023-05-05 18:41:35 +0900

    feat: save 메소드 로직 수정 및 인자 ExchangeRequestDto로 수정

--- src/main/java/com/party_grouping/service/ExchangeService.java
+++ src/main/java/com/party_grouping/service/ExchangeService.java
@@ -1,17 +1,14 @@
 package com.party_grouping.service;
 
 import com.party_grouping.api.ApiDnF;
-import com.party_grouping.dto.CharacterDto;
-import com.party_grouping.dto.ExchangeDto;
+import com.party_grouping.entity.CharacterEntity;
 import com.party_grouping.exception.ApiException;
 import com.party_grouping.repository.CharacterRepo;
 import com.party_grouping.repository.ExchangeRepo;
+import com.party_grouping.request.ExchangeRequestDto;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 @Transactional
@@ -26,29 +23,15 @@ public class ExchangeService {
         this.apiDnF = apiDnF;
     }
 
-    public String save(List<String> apiIdList) {
-        List<CharacterDto> characterDtoList = new ArrayList<>();
-        CharacterDto buffer = null;
-        for (String apiId: apiIdList) {
-            CharacterDto characterDto = characterRepo.findByApiIdOptDto(apiId)
-                    .orElseThrow(() -> { throw new ApiException("해당 캐릭터가 존재하지 않습니다.", 404); });
+    public String save(ExchangeRequestDto exchangeRequestDto) {
+        String server = exchangeRequestDto.getServer();
+        List<CharacterEntity> characterEntityList = new ArrayList<>();
 
-            if (characterDto.isBuffer()) {
-                buffer = characterDto;
-                continue;
-            }
-            characterDtoList.add(characterDto);
+        for (String apiId: exchangeRequestDto.getApiIdList()) {
+            characterEntityList.add(characterRepo.findByApiIdOptEntity(server, apiId)
+                    .orElseThrow(() -> { throw new ApiException("해당 캐릭터를 찾을 수 없습니다.", 400); }));
         }
-        characterDtoList.add(buffer);
 
-        ExchangeDto exchangeDto = ExchangeDto.builder()
-                .character1(characterDtoList.get(0))
-                .character2(characterDtoList.get(1))
-                .character3(characterDtoList.get(2))
-                .character4(characterDtoList.get(3))
-                .build();
-
-        return exchangeRepo.save(exchangeDto);
+        return exchangeRepo.save(characterEntityList);
     }
-
 }

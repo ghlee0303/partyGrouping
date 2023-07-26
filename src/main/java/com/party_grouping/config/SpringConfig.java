b995e0be77ca844361c6c4db7bd3bc898e37f8b0
ghlee0303 <rjs993530@gmail.com>
2023-07-26 16:55:07 +0900

    refactor: 사용하지 않는 클래스 제거

--- src/main/java/com/party_grouping/config/SpringConfig.java
+++ src/main/java/com/party_grouping/config/SpringConfig.java
@@ -28,97 +28,15 @@ public class SpringConfig {
     public CharacterRepo characterRepo() {
         return new CharacterRepo(jpaQueryFactory(), characterItemRepo());
     }
-
-    @Bean
-    public DungeonRepo dungeonRepo() {
-        return new DungeonRepo(jpaQueryFactory(), qDungeonEntity());
-    }
-
     @Bean
     public ExchangeRepo exchangeRepo() {
         return new ExchangeRepo(jpaQueryFactory(), characterRepo());
     }
 
     @Bean
-    public CharacterAndDungeonRepo characterAndDungeonRepo() {
-        return new CharacterAndDungeonRepo(jpaQueryFactory(), characterRepo(), dungeonRepo(), qCharacterAndDungeonEntity());
-    }
-
-    @Bean
-    public PartyRepo partyRepo() {
-        return new PartyRepo(jpaQueryFactory(), dungeonRepo(), groupRepo());
-    }
-
-    @Bean
-    public PartyAndCharacterRepo partyAndCharacterRepo() {
-        return new PartyAndCharacterRepo(
-                jpaQueryFactory(),
-                partyRepo(),
-                characterRepo(),
-                qPartyAndCharacterEntity(),
-                qCharacterAndDungeonEntity(),
-                qPartyEntity());
-    }
-
-    @Bean
-    public GroupRepo groupRepo() {
-        return new GroupRepo(jpaQueryFactory(), qGroupEntity());
-    }
-
-    @Bean
-    public GroupAndCharacterRepo groupAndCharacterRepo() {
-        return new GroupAndCharacterRepo(jpaQueryFactory(), groupRepo(), characterRepo(), qGroupAndCharacterEntity());
-    }
-    @Bean
     public CharacterItemRepo characterItemRepo() {
-        return new CharacterItemRepo(jpaQueryFactory(), qCharacterItemEntity());
+        return new CharacterItemRepo(jpaQueryFactory());
     }
-
-    @Bean
-    public QCharacterEntity qCharacterEntity() {
-        return new QCharacterEntity("characterEntity");
-    }
-
-    @Bean
-    public QDungeonEntity qDungeonEntity() {
-        return new QDungeonEntity("dungeonEntity");
-    }
-
-    @Bean
-    public QExchangeEntity qExchangeEntity() {
-        return new QExchangeEntity("exchangeEntity");
-    }
-
-    @Bean
-    public QGroupEntity qGroupEntity() {
-        return new QGroupEntity("groupEntity");
-    }
-
-    @Bean
-    public QPartyEntity qPartyEntity() {
-        return new QPartyEntity("partyEntity");
-    }
-
-    @Bean
-    public QCharacterAndDungeonEntity qCharacterAndDungeonEntity(){
-        return new QCharacterAndDungeonEntity("characterAndDungeonEntity");
-    }
-
-    @Bean
-    public QPartyAndCharacterEntity qPartyAndCharacterEntity() {
-        return new QPartyAndCharacterEntity("partyAndCharacterEntity");
-    }
-
-    @Bean
-    public QGroupAndCharacterEntity qGroupAndCharacterEntity() {
-        return new QGroupAndCharacterEntity("groupAndCharacterEntity");
-    }
-
-    @Bean
-    public QCharacterItemEntity qCharacterItemEntity() {
-        return new QCharacterItemEntity("characterItemEntity");
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
